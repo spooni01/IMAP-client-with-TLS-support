1da02a5435b679dc2f55d4e9efa07d37f0f3bcee
spooni01 <adamliziciar27@gmail.com>
2024-11-15 14:27:37 +0100

    Updated tests structures

--- test/unit/__init__.cpp
+++ test/unit/__init__.cpp
@@ -1,10 +1,10 @@
 #include <gtest/gtest.h>
 
-#include "../src/main.hpp"
+#include "../../src/main.hpp"
 
-#include "unit/test_exceptions.cpp"
-#include "unit/test_ArgsParser.cpp"
-#include "unit/test_AuthManager.cpp"
+#include "test_exceptions.cpp"
+#include "test_ArgsParser.cpp"
+#include "test_AuthManager.cpp"
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
